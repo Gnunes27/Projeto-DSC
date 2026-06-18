be10dbf705b920b10846005cbf87fba19c2e9824
Gnunes27 <gabrielgnda27@gmail.com>
2026-06-18 13:15:23 -0300

    Add initial Livraria Maven project and UI
    "Importação inicial da aplicação Livraria: adicionado o arquivo pom.xml do Maven (inclui a dependência do AbsoluteLayout), a classe de entrada principal Livraria.java, as telas de interface gráfica (GUI) em Swing com os arquivos .java/.form gerados para TelaInicialView e TelaLoginView, e os recursos de imagem sob src/main/resources/images

---
+++ Livraria/src/main/java/com/mycompany/livraria/Livraria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.livraria;
+
+/**
+ *
+ * @author Gnunes
+ */
+public class Livraria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
