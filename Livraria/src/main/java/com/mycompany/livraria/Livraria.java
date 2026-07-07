89bd146fc73b7727482ba146c662046f61370a0a
Bruno Rodrigues <blich260@gmail.com>
2026-07-07 18:24:34 -0300

    Update Livraria.java
    Foi adicionado os usuários e administrador do sistema;

--- Livraria/src/main/java/com/mycompany/livraria/Livraria.java
+++ Livraria/src/main/java/com/mycompany/livraria/Livraria.java
@@ -22,12 +22,55 @@ public class Livraria {
             //Se não encontrar cria um adm
             Pessoa adm = new Pessoa();
             adm.setNome("adm");
-            adm.setEmail("adm");
-            adm.setSenha("123");
+            adm.setEmail("Adm@livraria");
+            adm.setSenha("12345");
             adm.setAdm(true);
-            adm.setSaldo(666.00);
+            adm.setSaldo(1.00);
             
             pessoaController.register(adm);
+        }
+        
+        //Usuários
+        try{
+            pessoaController.search("2");
+        }catch (RuntimeException e){
+            //Se não encontrar cria um user
+            Pessoa user = new Pessoa();
+            user.setNome("Pedro");
+            user.setEmail("Pedro@Usuario");
+            user.setSenha("p123");
+            user.setAdm(false);
+            user.setSaldo(1000.00);
+            
+            pessoaController.register(user);
+        }
+        
+        try{
+            pessoaController.search("3");
+        }catch (RuntimeException e){
+            //Se não encontrar cria um user
+            Pessoa user = new Pessoa();
+            user.setNome("Nanda");
+            user.setEmail("Nanda@Usuario");
+            user.setSenha("n123");
+            user.setAdm(false);
+            user.setSaldo(1000.00);
+            
+            pessoaController.register(user);
+        }
+        
+        try{
+            pessoaController.search("4");
+        }catch (RuntimeException e){
+            //Se não encontrar cria um user
+            Pessoa user = new Pessoa();
+            user.setNome("Luis");
+            user.setEmail("Luis@Usuario");
+            user.setSenha("l123");
+            user.setAdm(false);
+            user.setSaldo(1000.00);
+            
+            pessoaController.register(user);
         }
         
         java.awt.EventQueue.invokeLater(new Runnable() {
